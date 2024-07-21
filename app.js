e313e8e59b2feef0d920ff2d3fa040d28bafacb7
deepak <deepakjack273001@gmail.com>
2024-07-21 14:28:40 +0530

    initial change

---
+++ app.js
@@ -0,0 +1,16 @@
+let img=document.querySelector("img");
+function myfunon(){
+    img.style.animationDuration=3+"s";
+}
+function myfunoff(){
+    img.style.animationDuration=0+"s";
+}
+function myfun2(){
+    img.style.animationDuration=1+"s";
+}
+function myfun3(){
+    img.style.animationDuration=0.5+"s";
+}
+function myfun4(){
+    img.style.animationDuration=0.1+"s";
+}
